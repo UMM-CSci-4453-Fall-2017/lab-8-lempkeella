642b34d861300bc5ceddc721c3f8590c32ad6701
Laverne Schrock <schr1230@morris.umn.edu>
2017-11-06 15:47:22 -0600

    Add some notes to express.js

--- express.js
+++ express.js
@@ -7,16 +7,24 @@ port = process.env.PORT || 1337;
 credentials.host='ids.morris.umn.edu'; //setup database credentials
 var databaseName = "schr1230";
 
+// Created a connection pool
 connectionPool = mysql.createPool(credentials); // setup the connection
 
+// Attempts to resolve all get requests by reading from /public before
+// trying other routes
 app.use(express.static(__dirname + '/public'));
+
+// ---------------------------------------------------------------------
+// The desired behaviours of the following routes is described in api.md
+// ---------------------------------------------------------------------
+
 
 app.get("/buttons",function(req,res){
   var sql = mysql.format('SELECT buttonID,`left`,top,width,invID,item AS label FROM ??.till_buttons,??.inventory WHERE invID = id', [databaseName,databaseName]);
   connectionPool.query(sql,(function(res){return function(err,rows,fields){
     if(err) {
       console.log("Error: ?", err);
-      res.sendStatus(500);
+      res.sendStatus(500);   // If any error occurs, it is probably not the client's fault
     } else {
       res.send(rows);
     }
@@ -28,7 +36,7 @@ app.get("/transaction" , function(req,res) {
 			" WHERE prices.id=itemId AND itemId=inventory.id GROUP BY itemId;", function(err,rows,field) {
     if(err) {
       console.log("Error: ?", err);
-      res.sendStatus(500);
+      res.sendStatus(500);  // If any error occurs, it is probably not the client's fault
     } else {
       res.send(rows);
     }
@@ -40,10 +48,10 @@ app.delete("/transaction/:itemId", function(req, res){
   connectionPool.query(mysql.format("DELETE FROM ??.transaction WHERE itemId = ?", [databaseName, itemId]), function(err, rows, fields){
     if(err) {
       console.log("Error: ?", err);
-      res.sendStatus(500);
+      res.sendStatus(500); // If any error occurs, it is probably not the client's fault
     } else {
       if(rows.affectedRows == 0) {
-        res.sendStatus(404);
+        res.sendStatus(404); // Can't delete nonexistent items
       } else {
        res.send('');
       }
@@ -55,7 +63,7 @@ app.delete("/transaction" , function(req, res) {
   connectionPool.query(mysql.format("TRUNCATE ??.transaction", databaseName), function(err,rows,fields) {
     if(err) {
       console.log("Error: ?", err);
-      res.sendStatus(500);
+      res.sendStatus(500); // If any error occurs, it is probably not the client's fault
     } else {
       res.send('');
     }
@@ -70,7 +78,7 @@ app.post("/transaction/:itemId" , function(req,res) {
       if(err.code == 'ER_NO_REFERENCED_ROW_2'){
          res.status(400).send('Invalid Item ID');
       } else {
-         res.sendStatus(500);
+         res.sendStatus(500); // If any OTHER error occurs, it is probably not the client's fault
       }
     } else {
       res.send('');
